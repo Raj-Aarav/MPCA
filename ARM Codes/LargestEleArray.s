882bdb62b2e1e3317e6947e1de22fab9c5c38f39
Aarav <132212970+Raj-Aarav@users.noreply.github.com>
2024-01-24 20:04:50 +0530

    Added largestEleArray.s

---
+++ ARM Codes/LargestEleArray.s
@@ -0,0 +1,22 @@
+//Largest element in an array
+
+.DATA
+A: .WORD 10,50,30,5
+
+.TEXT
+LDR R1,=A
+LDR R0,[R1]
+MOV R3,#0
+
+LOOP: LDR R2,[R1,#4]!
+CMP R0, R2
+BLT Less
+L1: ADD R3,R3,#1
+CMP R3,#4
+BNE LOOP
+B EXIT
+
+Less : MOV R0,R2
+B L1
+
+EXIT : SWI 0X011
